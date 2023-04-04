69faa285c631e3a59cee478e29a102b6f3691b15
hepeiyang <201250192@smail.nju.edu.cn>
2023-04-04 21:03:14 +0800

    lalala

--- src/main/java/com/example/b/controller/StudentController.java
+++ src/main/java/com/example/b/controller/StudentController.java
@@ -6,8 +6,7 @@ import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.StaxDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,22 +31,22 @@ public class StudentController {
         return xStream.toXML(studentMapper.selectList(null).size());
     }
 
-    @PostMapping("/students/add")
-    public void addStudent(@RequestBody String studentXML) {
+    @GetMapping("/students/add")
+    public void addStudent(@RequestParam String studentXml) {
         xStream.processAnnotations(Student.class);
-        Student student = (Student) xStream.fromXML(studentXML);
+        Student student = (Student) xStream.fromXML(studentXml);
         studentMapper.insert(student);
     }
 
-    @PostMapping("/students/delete")
-    public void deleteStudent(@RequestBody String studentXML) {
+    @GetMapping("/students/delete")
+    public void deleteStudent(@RequestParam String studentXml) {
         xStream.processAnnotations(Student.class);
-        Student student = (Student) xStream.fromXML(studentXML);
+        Student student = (Student) xStream.fromXML(studentXml);
         studentMapper.deleteById(student.getName());
     }
 
-    @PostMapping("/students/update")
-    public void updateStudent(@RequestBody String studentXml) {
+    @GetMapping("/students/update")
+    public void updateStudent(@RequestParam String studentXml) {
         xStream.processAnnotations(Student.class);
         Student student = (Student) xStream.fromXML(studentXml);
         studentMapper.updateById(student); //todo
