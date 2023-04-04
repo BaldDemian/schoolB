b9336edd26cb4eec48cc92b9b9d32b71fd1a7b9a
hepeiyang <201250192@smail.nju.edu.cn>
2023-04-04 21:32:03 +0800

    lalala

--- src/main/java/com/example/b/controller/AccController.java
+++ src/main/java/com/example/b/controller/AccController.java
@@ -6,7 +6,7 @@ import com.example.b.service.LoginService;
 import com.example.b.service.RegisterService;
 import com.example.b.utils.TokenUtil;
 import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.StaxDriver;
+import com.thoughtworks.xstream.io.Xml.StaxDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,21 +22,21 @@ public class AccController {
     RegisterService registerService;
 
     @GetMapping("/register")
-    public String register(@RequestParam String userAccountXML) {
+    public String register(@RequestParam String userAccountXml) {
         xStream.processAnnotations(Acc.class);
-        Acc acc = (Acc) xStream.fromXML(userAccountXML);
+        Acc acc = (Acc) xStream.fromXML(userAccountXml);
         Map<String, String> res = registerService.register(acc);
         return xStream.toXML(res);
     }
     @GetMapping("/login")
-    public String login(@RequestParam String userAccountXML) {
-        // 解析XML到User实例
+    public String login(@RequestParam String userAccountXml) {
+        // 解析Xml到User实例
         xStream.processAnnotations(Acc.class);
-        Acc acc = (Acc) xStream.fromXML(userAccountXML);
+        Acc acc = (Acc) xStream.fromXML(userAccountXml);
         System.out.println(acc);
         // 接收结果
         Map<String, String> res = loginService.login(acc);
-        // 将结果转换为XML返回
+        // 将结果转换为Xml返回
         return xStream.toXML(res);
     }
     @GetMapping("/checkToken")
--- src/main/java/com/example/b/controller/EnrollController.java
+++ src/main/java/com/example/b/controller/EnrollController.java
@@ -116,10 +116,10 @@ public class EnrollController {
     }
 
     @GetMapping("/courses_selection/find_by_sno")
-    public String findEnrollBySno(@RequestParam String studentXML) {
-        System.out.println(studentXML);
+    public String findEnrollBySno(@RequestParam String studentXml) {
+        System.out.println(studentXml);
         xStream.processAnnotations(Student.class);
-        Student student = (Student) xStream.fromXML(studentXML);
+        Student student = (Student) xStream.fromXML(studentXml);
         QueryWrapper<Enroll> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("学号", student.getSno());
         List<Enroll> enrollList = enrollMapper.selectList(queryWrapper);
